843f639638413b489cd23ecff6361b1efba802c3
Sasha Prokhorenko <djminikin@gmail.com>
2019-04-28 13:26:46 +0200

    Clean up main

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  App({Key key}) : super(key: key);
+
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
