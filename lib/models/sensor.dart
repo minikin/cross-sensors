b6e03fbb7c5c5db371dd51f6f898c21a09d42508
Sasha Prokhorenko <djminikin@gmail.com>
2019-05-02 11:07:32 +0200

    Update Home

--- lib/models/sensor.dart
+++ lib/models/sensor.dart
@@ -1,9 +1,9 @@
-library sensor;
+// library sensor;
 
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:built_value/serializer.dart';
+// import 'package:built_collection/built_collection.dart';
+// import 'package:built_value/built_value.dart';
+// import 'package:built_value/serializer.dart';
 
-part 'sensor.g.dart';
+// part 'sensor.g.dart';
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,15 +7,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _buttonColor = Colors.green;
+  var _buttonText = 'START';
+  var _buttonBressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cross Sensors'),
+        backgroundColor: Colors.teal,
       ),
       body: SafeArea(
-        child: Container(
-          child: Text('data'),
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            child: RaisedButton(
+              color: _buttonColor,
+              child: Text(
+                _buttonText,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+              onPressed: () {
+                _buttonBressed = !_buttonBressed;
+                setState(() {
+                  if (_buttonBressed) {
+                    _buttonText = "STOP";
+                    _buttonColor = Colors.red;
+                  } else {
+                    _buttonText = "START";
+                    _buttonColor = Colors.green;
+                  }
+                });
+              },
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(75)),
+            ),
+          ),
         ),
       ),
     );
